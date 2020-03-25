289fb81c8cddb74c8e9ed75b841a4b9e21d6e0b1
guanshuaijie <1845543324@qq.com>
2020-03-25 13:05:12 +0800

    clear

--- backups/2/js/allLoaction.js
+++
@@ -1,8 +0,0 @@
-function getAllTop(obj) {
-  var allTop = obj.offsetTop;
-  var currentObj = obj;
-  while(currentObj = currentObj.offsetParent) {
-    allTop += currentObj.offsetTop;
-  }
-  return allTop;
-}
